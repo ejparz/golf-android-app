78be02528e62cb84b61b175f42b15558fe053692
Eric <eric@driverbase.com>
2020-03-03 15:41:24 -0500

    Basic test and example

--- app/src/androidTest/java/com/ejparz/golf/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ejparz/golf/ExampleInstrumentedTest.java
@@ -1,12 +1,18 @@
 package com.ejparz.golf;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketFactory;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import javax.net.ssl.SSLContext;
 
 import static org.junit.Assert.*;
 
@@ -17,11 +23,56 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ejparz.golf", appContext.getPackageName());
+    @Test
+    public void testWebsocket() throws Exception {
+        long startTime = System.nanoTime();
+        connectIpLocal();
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;  //divide by 1000000 to get milliseconds.
+
+        Log.i("Test", "Connect took " + duration + "ms");
+    }
+
+    //Connect took 277 ms
+    public void connect() throws Exception{
+        SSLContext context = NaiveSSLContext.getInstance("TLS");
+
+        WebSocketFactory factory = new WebSocketFactory();
+        factory.setSSLContext(context);
+        factory.setVerifyHostname(false);
+
+        WebSocket ws = factory.createSocket("wss://echo.websocket.org");
+
+        ws.connect();
+    }
+
+    //Attempt to connect to echo ip
+    public void connectIp() throws Exception{
+        SSLContext context = NaiveSSLContext.getInstance("TLS");
+
+        WebSocketFactory factory = new WebSocketFactory();
+        factory.setSSLContext(context);
+        factory.setVerifyHostname(false);
+
+        WebSocket ws = factory.createSocket("wss://174.129.224.73:443/");
+
+        ws.connect();
+    }
+
+    public void connectIpLocal() throws Exception{
+        SSLContext context = NaiveSSLContext.getInstance("TLS");
+
+        WebSocketFactory factory = new WebSocketFactory();
+        factory.setSSLContext(context);
+        factory.setVerifyHostname(false);
+
+       //WebSocket ws = factory.createSocket("wss://192.168.0.15:63850");
+
+        WebSocket ws = factory.createSocket("wss://localhost:64860");
+
+        ws.connect();
     }
 }
+
---
+++ node_websocket/server.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+
+const server = https.createServer({
+  cert: fs.readFileSync('C://Users//eparz//go//Projects//personal_projects//node_websocket//server.cert'),
+  key: fs.readFileSync('C://Users//eparz//go//Projects//personal_projects//node_websocket//server.key')
+});
+const wss = new WebSocket.Server({ server });
+
+console.log("Started Server")
+
+wss.on('connection', function connection(ws) {
+  console.log("Init Connection");
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
+server.listen(function listening() {
+  //
+  // If the `rejectUnauthorized` option is not `false`, the server certificate
+  // is verified against a list of well-known CAs. An 'error' event is emitted
+  // if verification fails.
+  //
+  // The certificate used in this example is self-signed so `rejectUnauthorized`
+  // is set to `false`.
+  //
+  const ws = new WebSocket(`wss://localhost:${server.address().port}`, {
+    rejectUnauthorized: false
+  });
+
+  ws.on('open', function open() {
+    ws.send('All glory to WebSockets!');
+  });
+});
+
+console.log(`wss://localhost:${server.address().port}`);
